dc0c5bbb9a2a6ef8882878afb7784f7963dddff5
Mark Centoni <marklc44@gmail.com>
2014-08-31 12:18:42 -0700

    add user_challenge tests

--- thejar/spec/models/user_challenge_spec.rb
+++ thejar/spec/models/user_challenge_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe UserChallenge, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @challenge = Challenge.create(title: 'New', start_date: DateTime.now, end_date: DateTime.tomorrow, status: "done")
+    @user = User.create(first_name: 'Joe', last_name: 'Doe', email: 'joe@doe.com', phone_number: '+16505555555', img_url: 'some_url')
+  end
+
+  it "should allow user to be added to a challenge" do
+    @challenge.users << @user
+    expect(@challenge.users.size).to eql(1)
+    expect(@user.challenges.size).to eql(1)
+  end
+
+  it "should allow role to be added to user_challenge" do
+    @challenge.users << @user
+    user_challenge = @user.user_challenges.last
+    user_challenge.update_attributes(role: 'supporter')
+    expect(user_challenge.role).to eql('supporter')
+  end
 end
