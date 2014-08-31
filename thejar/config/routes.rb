d965c33bf8133a88045d23d498fc4aff934353f2
Mark Centoni <marklc44@gmail.com>
2014-08-31 14:27:10 -0700

    added users, challenges, messages controllers and routes

--- thejar/config/routes.rb
+++ thejar/config/routes.rb
@@ -1,10 +1,30 @@
 Rails.application.routes.draw do
 
+  get 'messages/index'
+
+  get 'messages/show'
+
+  get 'messages/new'
+
+  get 'messages/create'
+
+  get 'messages/edit'
+
+  get 'messages/update'
+
+  get 'messages/delete'
+
   root to: 'sites#index'
 
   get '/about', to: 'sites#about'
 
   get '/contact', to: 'sites#contact'
+
+  resources :users do
+    resources :challenges do
+      resources :messages
+    end
+  end
 
 
 end
