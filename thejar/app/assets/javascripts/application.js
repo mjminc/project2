6c36ce8ef1f9953ac7ac99565fee5c768bffeaf9
Mark Centoni <marklc44@gmail.com>
2014-09-01 21:44:46 -0700

    install momentjs cuz fullcalendarjs requires it

--- thejar/app/assets/javascripts/application.js
+++ thejar/app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require bootstrap-3.2.0.min
 //= require video
+//= require moment.min
 //= require fullcalendar.min
 //= require handlebars.runtime
 //= require_tree ./templates
