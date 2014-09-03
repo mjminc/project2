8aff7cf787f77c64d06bbaf299978383094eeb4f
rasbabbo <rasbabbo@yahoo.com>
2014-09-02 19:40:02 -0700

    fix merge

--- thejar/app/controllers/sessions_controller.rb
+++ thejar/app/controllers/sessions_controller.rb
@@ -8,11 +8,7 @@ class SessionsController < ApplicationController
 
   	if @user
   		session[:user_id]= @user.id
-<<<<<<< HEAD
   		redirect_to user_path(@user), notice: "logged in"
-=======
-  		redirect_to "/users/:id", notice: "logged in"
->>>>>>> 7fd8d4d6db9a48805859e83e1f026c7578844201
   	else
   		flash.now[:error] = "not logged in"
   		render :new
