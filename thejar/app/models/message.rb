166227d33032dda3d7fb021d6dc4e2ae6a39cbb1
joemeers82 <joemeers82@gmail.com>
2014-09-02 22:49:48 -0700

    earl

--- thejar/app/models/message.rb
+++ thejar/app/models/message.rb
@@ -4,4 +4,19 @@ class Message < ActiveRecord::Base
 
   # validations
   validates_presence_of :text
+
+
+has_attached_file :img_url, styles:{
+		thumb: '100x100>',
+		square: '200x200#',
+		medium: '300x300>'
+	},
+		:bucket => ENV['MESSAGE_BUCKET'],
+		:s3_credentials =>{
+			:access_key_id=> ENV['AMAZON_ACCESS_KEY_ID'],
+			:secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
+		}
+	 validates_attachment_content_type :img_url, :content_type=> /\Aimage\/.*\Z/
+
+
 end
