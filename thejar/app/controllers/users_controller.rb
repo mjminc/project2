e8cf0b3c77f5b534b39b5497daef6cc27e0da4e2
joemeers82 <joemeers82@gmail.com>
2014-09-04 14:38:43 -0700

    pre-pull

--- thejar/app/controllers/users_controller.rb
+++ thejar/app/controllers/users_controller.rb
@@ -35,11 +35,10 @@ class UsersController < ApplicationController
         if challenge_id != nil
           challenge = Challenge.find_by_id(challenge_id)
           user = User.find_by_email(new_user[:email])
-
-          @challenge.role => "supporter"
-          @challenge.is_accepted => true
+          # @challenge.role =>"supporter"
+          # @challenge.is_accepted =>true
           user.challenges << challenge
-          
+
         end
 
 
