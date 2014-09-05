37d10e94fa3a9722f2d259330cc701ed40dd30cd
rasbabbo <rasbabbo@yahoo.com>
2014-09-04 17:44:04 -0700

    getting twilio up

--- thejar/app/controllers/challenges_controller.rb
+++ thejar/app/controllers/challenges_controller.rb
@@ -54,6 +54,8 @@ class ChallengesController < ApplicationController
     @user = current_user
     @user.challenges << @challenge
     # binding.pry
+    # number_to_send_to = params[:number_to_send_to]
+    send_text_message("6505337957")
     redirect_to "/users/#{@user.id}/challenges/#{@challenge.id}"
 
   end
@@ -89,4 +91,25 @@ class ChallengesController < ApplicationController
     duration[:pct_complete] = (((today - start_date).to_f / (end_date - start_date).to_f) * 100).round
     return duration
   end
+
+  def send_text_message(number_to_send_to)
+
+    twilio_sid = ENV['ACCOUNT_SID']
+    twilio_token = ENV['AUTH_TOKEN']
+
+    puts "WE GETTING ANYTHING??"
+    puts twilio_sid
+    puts twilio_token
+
+    twilio_phone_number = "6502760630"
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+    @twilio_client.account.sms.messages.create(
+      :from => "16502760630",
+      :to => number_to_send_to,
+      :body => "You have been invited to join a challenge!  Go to ......blah"
+      )
+  end
+
 end
