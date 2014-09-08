ce6dc79b652a2ec004c2ab3acd9c1059040c6d35
Mark Centoni <marklc44@gmail.com>
2014-09-08 16:06:18 -0700

    factories folders added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject { FactoryGirl.create(:user)}
+
+  it "should have a valid factory" do
+    expect(subject).to be_valid
+  end
 end
